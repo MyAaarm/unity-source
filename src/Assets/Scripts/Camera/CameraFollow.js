7778e15cdf57f93c47d4f03862b9b66546869346
Jakob Svensson <jakob-_-92@hotmail.com>
2015-01-30 16:12:15 +0100

    Set the camera to follow the two players

--- src/Assets/Scripts/Camera/CameraFollow.js
+++ src/Assets/Scripts/Camera/CameraFollow.js
@@ -1,22 +1,44 @@
 ﻿#pragma strict
 
-var target : Transform;            // The position that that camera will be following.
+var target1 : Transform;         
+var target2 : Transform;            // The position that that camera will be following.
 var smoothing : float = 5f;        // The speed with which the camera will be following.
+var height : float = 10f;
 
 private var offset : Vector3;                     // The initial offset from the target.
 
 function Start ()
 {
     // Calculate the initial offset.
-    offset = transform.position - target.position;
+    offset = transform.position - (target1.position + target2.position)/2;
 }
 
 
 function FixedUpdate ()
-{
+{	
+	var centerPoint = (target1.position + target2.position)/2;
+	
+	var distance = Vector3.Distance(target1.position, target2.position);
+	
+	height = Mathf.Lerp(height,distance,Time.deltaTime);
+	if(height <5f){
+		height = 5f;
+	}
+	
+	transform.position.x = centerPoint.x ;
+ 
+	transform.position.z = centerPoint.z - distance;
+
+	transform.position.y = height;
+	
+	
+	
+	transform.LookAt(centerPoint);
+	
+	
     // Create a postion the camera is aiming for based on the offset from the target.
-    var targetCamPos : Vector3 = target.position + offset;
+   // var targetCamPos : Vector3 = (target1.position + target2.position)/2 + offset;
 
     // Smoothly interpolate between the camera's current position and it's target position.
-    transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+   // transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
 }
