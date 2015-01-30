f39b5fd659eda3276f394911d1633b9fb0efd0cd
Jakob Svensson <jakob-_-92@hotmail.com>
2015-01-30 16:17:08 +0100

    Cleanup of CameraFollow.js

--- src/Assets/Scripts/Camera/CameraFollow.js
+++ src/Assets/Scripts/Camera/CameraFollow.js
@@ -2,17 +2,9 @@
 
 var target1 : Transform;         
 var target2 : Transform;            // The position that that camera will be following.
-var smoothing : float = 5f;        // The speed with which the camera will be following.
+var cameraSpeed : float = 5f;        // The speed with which the camera will be following.
 var height : float = 10f;
-
-private var offset : Vector3;                     // The initial offset from the target.
-
-function Start ()
-{
-    // Calculate the initial offset.
-    offset = transform.position - (target1.position + target2.position)/2;
-}
-
+                  
 
 function FixedUpdate ()
 {	
@@ -20,7 +12,7 @@ function FixedUpdate ()
 	
 	var distance = Vector3.Distance(target1.position, target2.position);
 	
-	height = Mathf.Lerp(height,distance,Time.deltaTime);
+	height = Mathf.Lerp(height,distance,Time.deltaTime*cameraSpeed);
 	if(height <5f){
 		height = 5f;
 	}
@@ -33,12 +25,6 @@ function FixedUpdate ()
 	
 	
 	
-	transform.LookAt(centerPoint);
-	
-	
-    // Create a postion the camera is aiming for based on the offset from the target.
-   // var targetCamPos : Vector3 = (target1.position + target2.position)/2 + offset;
+	transform.LookAt(centerPoint);	
 
-    // Smoothly interpolate between the camera's current position and it's target position.
-   // transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
 }
