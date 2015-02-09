caf9894db3a7dfd1469f641421080ac260701411
JohanF <johan.fredriksson@outlook.com>
2015-02-09 21:39:54 +0100

    added Perlin generated terrain

--- src/Assets/Scripts/Level/World.cs
+++ src/Assets/Scripts/Level/World.cs
@@ -10,16 +10,16 @@ public class World : MonoBehaviour {
     void Start()
     {
 
-        for (int x = -2; x < 2; x++)
-        {
-            for (int y = -1; y < 1; y++)
-            {
-                for (int z = -1; z < 1; z++)
-                {
-                    CreateChunk(x * 16, y * 16, z * 16);
-                }
-            }
-        }
+		for (int x = -4; x < 4; x++)
+		{
+			for (int y = -1; y < 3; y++)
+			{
+				for (int z = -4; z < 4; z++)
+				{
+					CreateChunk(x * 16, y * 16, z * 16);
+				}
+			}
+		}
     }
 
 	// Update is called once per frame
@@ -44,7 +44,7 @@ public class World : MonoBehaviour {
 
         //Add it to the chunks dictionary with the position as the key
         chunks.Add(worldPos, newChunk);
-
+		/*
         for (int xi = 0; xi < 16; xi++)
         {
             for (int yi = 0; yi < 16; yi++)
@@ -64,6 +64,13 @@ public class World : MonoBehaviour {
                 }
             }
         }
+        */
+		var terrainGen = new TerrainGen();
+		newChunk = terrainGen.ChunkGen(newChunk);
+		
+		//newChunk.SetBlocksUnmodified();
+		
+		//bool loaded = Serialization.Load(newChunk);
     }
 
     public void DestroyChunk(int x, int y, int z)
@@ -119,6 +126,24 @@ public class World : MonoBehaviour {
         {
             chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, block);
             chunk.update = true;
+
+			UpdateIfEqual(x - chunk.pos.x, 0, new WorldPos(x - 1, y, z));
+			UpdateIfEqual(x - chunk.pos.x, Chunk.chunkSize - 1, new WorldPos(x + 1, y, z));
+			UpdateIfEqual(y - chunk.pos.y, 0, new WorldPos(x, y - 1, z));
+			UpdateIfEqual(y - chunk.pos.y, Chunk.chunkSize - 1, new WorldPos(x, y + 1, z));
+			UpdateIfEqual(z - chunk.pos.z, 0, new WorldPos(x, y, z - 1));
+			UpdateIfEqual(z - chunk.pos.z, Chunk.chunkSize - 1, new WorldPos(x, y, z + 1));
+
         }
     }
+
+	void UpdateIfEqual(int value1, int value2, WorldPos pos)
+	{
+		if (value1 == value2)
+		{
+			Chunk chunk = GetChunk(pos.x, pos.y, pos.z);
+			if (chunk != null)
+				chunk.update = true;
+		}
+	}
 }
