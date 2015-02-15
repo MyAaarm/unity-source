47301c124e2cba92820177f8ee5e6e57ec917c62
Samir Chaabi <chaabi@student.chalmers.se>
2015-02-15 21:14:25 +0100

    Spawn crates and tinySkeleton

---
+++ src/Assets/Scripts/Crate.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+public var tinySkeleton : GameObject;
+private var timer: float = 30;
+
+//Transforms & Rotates the crate 
+function Update () {
+	transform.Rotate(new Vector3(15, 30,45) * Time.deltaTime);
+//	if (transform.position.y < 20 && transform.position.y > 1){
+//		transform.position.y -= 0.05;
+//	}
+//	else if(transform.position.y > 1){
+//		transform.position.y -= 0.2;
+//	}
+	OldCrate();
+//	this.renderer.material.color.a = 1.0f;
+}
+
+//Removes crate after 'x' amount of time
+function OldCrate(){
+	timer -= Time.deltaTime;
+	if (timer <= 0){
+		DestroyCrate();
+	}
+}
+
+//The crate is destroyed, the crate count decreases
+function DestroyCrate(){
+	Destroy(this.gameObject);
+	spawnCrates.nbrOfCrates --;
+}
+
+//When player picks up the crate the content is instantiated 
+function OnTriggerEnter (other : Collider) {
+	if(other.tag == "Player"){
+		DestroyCrate();
+		Instantiate (tinySkeleton, transform.position, Quaternion.identity);
+	}
+}
---
+++ src/Assets/Scripts/spawnCrates.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+public var Crate : GameObject;
+private var timer: float;
+public var crateSpawnHeight : float;
+public var timeOk : boolean;
+static var nbrOfCrates : float = 0;
+
+function Update () {
+	setTimer();
+//	Debug.Log();
+}
+
+function setTimer(){
+	if (nbrOfCrates == 0){ //If no crates available - count down the timer
+		timer -= Time.deltaTime;
+		if(timer <= 0){
+			timeOk = Time.realtimeSinceStartup > 2; //To not spawn crates directly
+			timer = Random.Range(5, 10); //Crate spawn-timer (between 60 and 90 sec)
+			spawnCrate(timeOk);
+		}
+	}
+}
+//Spawns crates and increases the crate count (no more than 1 at a time)
+function spawnCrate(timeOk : boolean){
+	if (timeOk && nbrOfCrates == 0){
+		Instantiate (Crate, Vector3(Random.Range(1, 15), crateSpawnHeight, Random.Range(1, 15)), Quaternion.identity);
+		nbrOfCrates ++;
+	}
+}
