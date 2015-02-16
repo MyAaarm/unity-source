604f5e940666c05e7edee32035c450d7eaf56c5c
Pierre Reimertz <pierre@pierrereimertz.com>
2015-02-16 09:09:42 +0100

    presentation menu

--- src/Assets/Scripts/HomeMenuHandler.js
+++ src/Assets/Scripts/HomeMenuHandler.js
@@ -4,6 +4,10 @@
  private var selection:GameObject;
  private var selectionNum:float = 0;
  private var isChanging = false;
+ private var isOnlyBack = false;
+
+ private var helpText;
+
 
 function Start () {
   menu.push(GameObject.Find("Buttons/play"));
@@ -11,26 +15,97 @@ function Start () {
   menu.push(GameObject.Find("Buttons/help"));
   menu.push(GameObject.Find("Buttons/exit"));
 
+  helpText = GameObject.Find("helpText");
+
 
   menu[0].GetComponent(UI.Image).color = Color.black;
 }
 
 function Update () {
   transform.rotation = Quaternion.Euler(Mathf.Sin(Time.realtimeSinceStartup) * 3, Mathf.Sin(Time.realtimeSinceStartup) * 7, 0);
+  Debug.Log(selectionNum);
+
+  if(Input.GetKeyDown("joystick 1 button 16") || Input.GetKeyDown("space")) {
+    HandleMenuClick();
+  }
+
+  if(isOnlyBack) return;
 
   if(Input.GetAxis("Vertical") < 0 && isChanging == false) {
     UpdateMenu(1);
   }
   else if(Input.GetAxis("Vertical") > 0 && isChanging == false) {
     UpdateMenu(-1);
+  }
+
+  else if(Input.GetAxisRaw("PS3LeftJoystickYOSX1") < 0 && isChanging == false) {
+    UpdateMenu(1);
+  }
+  else if(Input.GetAxisRaw("PS3LeftJoystickYOSX1") > 0 && isChanging == false) {
+    UpdateMenu(-1);
+  }
+
+
+
+}
+
+function HandleMenuClick() {
+
+  if(selectionNum == 0) {
+    Application.LoadLevel("Play");
+  }
+  else if(selectionNum == 1) {
+
+  }
+  else if(selectionNum == 2) {
+    isOnlyBack = true;
+
+    menu[0].GetComponentInChildren(UI.Text).color = Color(0,0,0,0);
+    menu[1].GetComponentInChildren(UI.Text).color = Color(0,0,0,0);
+
+    menu[2].GetComponentInChildren(UI.Text).color = Color(0,0,0,0);
+    menu[2].GetComponent(UI.Image).color = Color(0,0,0,0);
+
+    menu[3].GetComponentInChildren(UI.Text).text = 'BACK';
+    menu[3].GetComponent(UI.Image).color = Color.black;
+
+    helpText.GetComponent(UI.Text).color = Color.white;
+
+    selectionNum = 3;
+  }
+  else if(selectionNum == 3) {
+    if(isOnlyBack == true) {
+      ResetMenu(2);
+    }
+    else {
+      Application.Quit();
+    }
 
   }
 }
 
+function ResetMenu (selectedMenuItem) {
+    selectionNum = selectedMenuItem;
+
+    menu[0].GetComponentInChildren(UI.Text).color = Color.white;
+    menu[1].GetComponentInChildren(UI.Text).color = Color.white;
+    menu[2].GetComponentInChildren(UI.Text).color = Color.white;
+    menu[3].GetComponentInChildren(UI.Text).color = Color.white;
+
+    menu[selectedMenuItem].GetComponent(UI.Image).color = Color.black;
+
+    menu[3].GetComponent(UI.Image).color = Color(0,0,0,0);
+    menu[3].GetComponentInChildren(UI.Text).text = 'EXIT';
+
+    helpText.GetComponent(UI.Text).color = Color(0,0,0,0);
+    isOnlyBack = false;
+}
+
 function UpdateMenu (value) {
+  if(selectionNum + value >= menu.length || selectionNum + value < 0) return;
+
   isChanging = true;
 
-  if(selectionNum + value >= menu.length || selectionNum + value < 0) return;
   Debug.Log('value:' + value);
 
   menu[selectionNum].GetComponent(UI.Image).color = Color(0,0,0,0);
@@ -41,3 +116,4 @@ function UpdateMenu (value) {
   yield WaitForSeconds (0.2);
   isChanging = false;
 }
+
--- src/Assets/Scripts/Player/PlayerHealth.js
+++ src/Assets/Scripts/Player/PlayerHealth.js
@@ -6,11 +6,23 @@ var currentHealth : int;                                    // The current healt
 private var isDead : boolean;                                                // Whether the player is dead.
 private var damaged : boolean;                                               // True when the player gets damaged.
 
+private var p1Health: GameObject;
+private var p2Health: GameObject;
+private var won: GameObject;
+
 
 function Awake ()
 {
     // Set the initial health of the player (Maybe cheat a little and add more health to your character, only an if statement away).
     currentHealth = startingHealth;
+    p1Health = GameObject.Find("player1Health");
+    p2Health = GameObject.Find("player2Health");
+    won = GameObject.Find("won");
+}
+
+function Start ()
+{
+    // Set the initial health of the player (Maybe cheat a little and add more health to your character, only an if statement away).
 }
 
 
@@ -34,11 +46,19 @@ function Update ()
 
 public function TakeDamage (amount : int)
 {
-    // Set the damaged flag 
+    // Set the damaged flag
     damaged = true;
 
     // Reduce the current health by the damage amount.
     currentHealth -= amount;
+
+    if(this.name == 'Player 2'){
+        p1Health.GetComponent(UI.Text).text = 'p1:' + currentHealth;
+    }
+    else {
+        p2Health.GetComponent(UI.Text).text = 'p2:' + currentHealth;
+    }
+
 
     // If the player has lost all it's health and the death flag hasn't been set yet...
     if(currentHealth <= 0 && !isDead)
@@ -47,7 +67,7 @@ public function TakeDamage (amount : int)
         Death ();
     }
     Debug.Log("Damage taken, Current health for " + this.name + ": "+ currentHealth);
-    
+
 }
 
 
@@ -56,6 +76,16 @@ function Death ()
     // Set the death flag so this function won't be called again (you wouldn't want to die twice would you?).
     isDead = true;
 
+    if(this.name == 'Player 1'){
+        won.GetComponent(UI.Text).text = 'player 1 won!';
+    }
+    else {
+        won.GetComponent(UI.Text).text = 'player 2 won!';
+    }
+
+    yield WaitForSeconds(5);
+    Application.LoadLevel('Play');
+
    //Do dead stuff that dead players do
-   
+
 }
