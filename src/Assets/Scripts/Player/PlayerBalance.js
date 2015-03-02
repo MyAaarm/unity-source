f298f51a77874ec5a1b2252c8af2c22156e12c79
JohanF <johan.fredriksson@outlook.com>
2015-03-02 22:56:02 +0100

    Initial falldown testing

---
+++ src/Assets/Scripts/Player/PlayerBalance.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+function FixedUpdate(){
+	Debug.Log(this.transform.eulerAngles.y);
+	if(Mathf.Abs(this.transform.eulerAngles.x) > 15 || Mathf.Abs(this.transform.eulerAngles.z) > 15){
+		Debug.Log("FALLEN PLAYER");
+	}
+}
