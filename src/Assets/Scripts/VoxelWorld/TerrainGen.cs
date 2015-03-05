b79c447a3a851ee811fb7d9b295db17099741615
Johan Fredriksson <johfr@student.chalmers.se>
2015-03-05 17:49:11 +0100

    Added gates of hell

--- src/Assets/Scripts/VoxelWorld/TerrainGen.cs
+++ src/Assets/Scripts/VoxelWorld/TerrainGen.cs
@@ -2,21 +2,23 @@
 using System.Collections;
 
 public class TerrainGen : MonoBehaviour {
-
+	
 	/*
 	 * ******** HILL WORLD *********/
 	float stoneBaseHeight = 1;
 	float stoneBaseNoise = 0.05f;
 	float stoneBaseNoiseHeight = 4;
 	
-	float stoneMountainHeight = 15;
+	float stoneMountainHeight = 1;
 	float stoneMountainFrequency = 0.008f;
 	float stoneMinHeight = -12;
 	
 	float dirtBaseHeight = 3;
 	float dirtNoise = 0.04f;
 	float dirtNoiseHeight = 1;
-
+	
+	int l = 1;
+	
 	/*
 	 * ******** FLAT WORLD ********
 	 * float stoneBaseHeight = 1;
@@ -30,13 +32,17 @@ public class TerrainGen : MonoBehaviour {
 	float dirtBaseHeight = 3;
 	float dirtNoise = 0.04f;
 	float dirtNoiseHeight = 1;*/ 
-
+	
 	public Chunk ChunkGen(Chunk chunk)
 	{
 		for (int x = chunk.pos.x; x < chunk.pos.x + Chunk.chunkSize; x++)
 		{
+			
+			
 			for (int z = chunk.pos.z; z < chunk.pos.z + Chunk.chunkSize; z++)
-			{
+			{	
+				//Debug.Log (" x == " + (((Mathf.Acos((x)/(Mathf.Sqrt(Mathf.Pow(x,2)+Mathf.Pow (z,2))))) > (Mathf.PI/3)) && ((Mathf.Acos((x)/(Mathf.Sqrt(Mathf.Pow(x,2)+Mathf.Pow (z,2))))) < (Mathf.PI/6))));
+				
 				chunk = ChunkColumnGen(chunk, x, z);
 			}
 		}
@@ -47,7 +53,7 @@ public class TerrainGen : MonoBehaviour {
 	{
 		return Mathf.FloorToInt( (SimplexNoise.Noise.Generate(x * scale, y * scale, z * scale) + 1f) * (max/2f));
 	}
-
+	
 	public Chunk ChunkColumnGen(Chunk chunk, int x, int z)
 	{
 		int stoneHeight = Mathf.FloorToInt(stoneBaseHeight);
@@ -60,19 +66,35 @@ public class TerrainGen : MonoBehaviour {
 		
 		int dirtHeight = stoneHeight + Mathf.FloorToInt(dirtBaseHeight);
 		dirtHeight += GetNoise(x, 100, z, dirtNoise, Mathf.FloorToInt(dirtNoiseHeight));
-		
+		int k = 0;
+		int p = 0;
 		for (int y = chunk.pos.y; y < chunk.pos.y + Chunk.chunkSize; y++)
-		{
-			if (y <= stoneHeight)
-			{
-				chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new Block());
+		{	
+			k = k + Random.Range(-1,1);
+			
+			/*if(p > 120){ 
+				l = l + 1;
+				if(l>3) l = 0;
+				p = 0;
 			}
-			else if (y <= dirtHeight)
-			{
-				chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockGrass());
-			}
-			else
-			{
+			
+			p++;*/
+			
+			if(!(((x-(z)) <= (18+k)*2) && ((x-(z)) >= (10+k)*2))){
+				//if(!(((x-(z)*(1/l)) <= (18+k)*2) && ((x-(z)*(1/l)) >= (10+k)*2))){
+				if (y <= stoneHeight)
+				{
+					chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new Block());
+				}
+				else if (y <= dirtHeight)
+				{
+					chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockGrass());
+				}
+				else
+				{
+					chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockAir());
+				}
+			} else {
 				chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockAir());
 			}
 			
@@ -80,5 +102,5 @@ public class TerrainGen : MonoBehaviour {
 		
 		return chunk;
 	}
-
+	
 }
