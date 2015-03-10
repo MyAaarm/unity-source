3240b19f9340da3aa7e876eb24657361693cd7dd
Jakob Svensson <jakob-_-92@hotmail.com>
2015-03-10 13:12:47 +0100

    Removed debug messages

--- src/Assets/Scripts/Controllers/GameController.js
+++ src/Assets/Scripts/Controllers/GameController.js
@@ -10,7 +10,7 @@ public var Player : GameObject;
 public var Judge : GameObject;
 
 static var players : GameObject[];
-static var playerControllers = ['Keyboard', 'Keyboard', 'PS3OSX', 'PS3OSX'];
+static var playerControllers = ['Keyboard', 'Keyboard', 'Keyboard', 'PS3OSX'];
 
 function Awake () {
   DontDestroyOnLoad(this);
@@ -30,7 +30,6 @@ function Start () {
 function setGameControllers(){
 	var gameControllers : String[] = Input.GetJoystickNames();
 	for(var i = 0; i < gameControllers.Length; i++){
-		Debug.Log(gameControllers[i]);
 		if(gameControllers[i]=="XBOX 360 For Windows (Controller)"){
 			playerControllers[i] = "X360PC";
 		}else if(gameControllers[i]=="©Microsoft Corporation Xbox 360 Wired Controller"){
